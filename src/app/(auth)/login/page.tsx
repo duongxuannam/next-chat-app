dffdd20afdb7131a848f09a08fcde84806307fbd
Xuan Nam <duongxuannam1995@gmail.com>
2023-06-15 18:48:23 +0700

    trigger build

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -25,7 +25,7 @@ export default function Login() {
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full flex flex-col items-center max-w-md space-y-8">
           <div className="flex flex-col items-center gap-8">
-            logo
+            logo {process.env.NEXTAUTH_URL} NEXTAUTH_URL
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
               Sign in to your accountt
             </h2>
